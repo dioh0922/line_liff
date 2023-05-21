e9df737499d6d38ad57a0d66907577c423cdfb6e
dioh0922 <diohound@gmail.com>
2023-05-21 21:46:05 +0900

    リッチメニューの作成バッチ追加

---
+++ line_setting/curl_batch/create_richmenu_batch.php
@@ -0,0 +1,24 @@
+<?php
+    require_once(dirname(__FILE__)."/../../vendor/autoload.php");
+    require_once("./RichMenuSetting.php");
+
+    $env = Dotenv\Dotenv::createImmutable(dirname(__FILE__)."/../../../env");
+    $env->load();
+
+
+    $disp = [];
+
+    $setting = new RichMenuSetting();
+    $richMenuId = $setting->createMenu();
+
+    $disp[] = $richMenuId;
+    
+    $disp[] = "putImg:".$setting->putMenuImg($richMenuId);
+    
+    $disp[] = "setDefault:".$setting->setDefaultMenu($richMenuId);
+
+    $disp[] = "getDefault:".$setting->getDefaultMenu();
+
+    echo implode("<br>", $disp);
+
+?>
