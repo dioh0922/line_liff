7a5ad1d35bd133db0a474d9c0c4dbbed752eb8e2
dioh0922 <diohound@gmail.com>
2023-06-10 21:40:19 +0900

    vuetify化

--- liff/credit/credit.js
+++ liff/credit/credit.js
@@ -14,7 +14,7 @@ function submit(){
     post_data.append("value", pay);
     post_data.append("detail", detail);
     axios.post("./save.php", post_data).then(res => {
-        document.getElementById("close").className = "row ms-1 mb-3";
+        document.getElementById("close").className = "row";
     }).catch(er => {
 
     }).finally(() => {
@@ -23,6 +23,61 @@ function submit(){
     });
 }
 
-function done(){
-    liff.closeWindow();
-}
+
+(window.onload = () => {
+  const { createApp } = Vue;
+  const vuetify = Vuetify.createVuetify({
+    theme: {
+ 
+    }
+  });
+
+  createApp({
+    data() {
+      return {
+        pay: 0,
+        detail: "",
+        loading: false,
+        done: false,
+        dialog: {
+            msg: "",
+            disp: false
+        }
+      }
+    },
+    methods:{
+      submit(){
+        let post = new FormData();
+        post.append("destination", this.text);
+        if(parseInt(this.pay) <= 0 || this.detail === ""){
+            this.openDialog("内容を入力");
+            return ;
+        }
+        this.loading = true;
+
+        let post_data = new FormData();
+        post_data.append("value", this.pay);
+        post_data.append("detail", this.detail);
+        axios.post("./save.php", post_data).then(res => {
+            this.done = true;
+        }).catch((er) => {
+            this.openDialog(er);
+        }).finally(() => {
+            this.loading = false;
+        });
+      },
+      done() {
+        liff.closeWindow();
+      },
+      openDialog(msg){
+        this.dialog.msg = msg;
+        this.dialog.disp = true;
+        this.loading = false;
+      },
+      closeDialog(){
+        this.dialog.msg = "";
+        this.dialog.disp = false;
+      }
+    }
+  }).use(vuetify).mount('#container');
+});
