a036412e97d66b808c530885da2c94af905b7ff4
dioh0922 <diohound@gmail.com>
2023-06-10 21:42:41 +0900

    処理廃止

--- liff/credit/credit.js
+++ liff/credit/credit.js
@@ -1,29 +1,4 @@
 
-function submit(){
-
-    let pay = parseInt(document.getElementById("pay-value").value);
-    let detail = document.getElementById("pay-detail").value;
-    if(parseInt(pay) <= 0 || detail === ""){
-        return ;
-    }
-
-    document.getElementById("btn-load").className = "";
-    document.getElementById("btn-content").innerHTML = "Loading...";
-
-    let post_data = new FormData();
-    post_data.append("value", pay);
-    post_data.append("detail", detail);
-    axios.post("./save.php", post_data).then(res => {
-        document.getElementById("close").className = "row";
-    }).catch(er => {
-
-    }).finally(() => {
-        document.getElementById("btn-load").className = "fade";
-        document.getElementById("btn-content").innerHTML = "送信";
-    });
-}
-
-
 (window.onload = () => {
   const { createApp } = Vue;
   const vuetify = Vuetify.createVuetify({
