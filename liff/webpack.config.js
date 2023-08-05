ea18d0694d5aadbd11ca5451687d9e4ac1d96ecd
dioh0922 <diohound@gmail.com>
2023-08-05 11:58:25 +0900

    ビルド時間のためにコンパイルのみに

--- liff/webpack.config.js
+++ liff/webpack.config.js
@@ -32,6 +32,7 @@ module.exports = {
         test: /\.ts$/,
         loader: "ts-loader",
         options: {
+          transpileOnly:true,
           appendTsSuffixTo: [/\.vue$/]
         }
       },
